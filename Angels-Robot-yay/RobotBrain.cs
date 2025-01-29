73ffd0290303ea0383249b9d64abd68978060b0b
Angel Zeng <angelzeng08@hotmail.com>
2025-01-29 17:11:23 +0100

    remove Robot Brain

--- Angels-Robot-yay/RobotBrain.cs
+++ Angels-Robot-yay/RobotBrain.cs
@@ -10,29 +10,51 @@
 //     public int state;
 //     public void RunState()
 //     {
-//         switch(state) {
+//         switch (state)
+//         {
+//             case 0:
+//                 state = 1;
+//                 break;
 //             case 1:
 //                 DriveForward();
 //                 break;
 //             case 2:
 //                 EmergencyStop();
 //                 break;
-//             case 3: 
-                
+//             case 3:
+
 //         }
 //     }
 
 //     private void DriveForward()
 //     {
-//         if (knoppie.ButtonPressed())
+//         AfstandsSensor afstandsSensor = new AfstandsSensor(16);
+
+//         RobotRijden driveSystem = new RobotRijden();
+//         driveSystem.TargetSpeed = 0.2;
+//         driveSystem.Update();
+
+//         if (afstandsSensor.BotsingsGevaar())
 //         {
-//             state = 2;
-//             return;
+//             driveSystem.EmergencyStop();
+//             // speaker.PlaySequence();
+//             while (!afstandsSensor.VeiligeAfstand())
+//             {
+//                 Robot.Wait(100);
+//             }
+//             driveSystem.TargetSpeed = 0.2;
 //         }
+
+
+//         // if (knoppie.ButtonPressed())
+//         // {
+//         //     state = 2;
+//         //     return;
+//         // }
 //     }
 
 //     private void EmergencyStop()
 //     {
 
-//     }   
+//     }
 // }
